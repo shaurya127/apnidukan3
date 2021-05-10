b6cea6e6557dd93657a4e322ce4883fbede61545
shaurya <2019uee1692@mnit.ac.in>
2021-05-10 11:02:07 +0530

    updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -81,6 +81,27 @@
     onscroll(document, toggleBacktotop);
   }
 
+  on("click", ".mobile-nav-toggle", function (e) {
+    select("#navbar").classList.toggle("navbar-mobile");
+    this.classList.toggle("bi-list");
+    this.classList.toggle("bi-x");
+  });
+
+  /**
+   * Mobile nav dropdowns activate
+   */
+  on(
+    "click",
+    ".navbar .dropdown > a",
+    function (e) {
+      if (select("#navbar").classList.contains("navbar-mobile")) {
+        e.preventDefault();
+        this.nextElementSibling.classList.toggle("dropdown-active");
+      }
+    },
+    true
+  );
+
   /**
    * Mobile nav dropdowns activate
    */
